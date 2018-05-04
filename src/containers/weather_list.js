730e432a0536010898bd8671bf3583aba3159d83
Matt Upham <mupham@twilio.com>
2018-05-03 17:43:15 -0700

    create static weather list to display on page with completed table

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+
+class WeatherList extends Component {
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
-          
+
         </tbody>
       </table>
     );
   }
 }
+
+function mapStateToProps({ weather }) {
+  // arg is similar to const weather = state.weather
+  return { weather };
+  // { weather } === { weather: weather }
+}
+
+// export connected version of WeatherList
+export default connect(mapStateToProps)(WeatherList);
